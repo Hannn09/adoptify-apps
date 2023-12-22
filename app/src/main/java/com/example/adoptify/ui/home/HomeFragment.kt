0e8c5f1933537e2178695302939efc67f635db12
rezallna <rezaliana@student.uns.ac.id>
2023-12-22 15:30:38 +0700

    revisi donation

--- app/src/main/java/com/example/adoptify/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/adoptify/ui/home/HomeFragment.kt
@@ -2,26 +2,26 @@ package com.example.adoptify.ui.home
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.adoptify.R
+import androidx.viewpager2.widget.ViewPager2
 import com.example.adoptify.databinding.FragmentHomeBinding
 import com.example.adoptify.model.dummyBanner
 import com.example.adoptify.ui.donation.Donation
 import com.example.adoptify.ui.login.LoginActivity
 import com.example.adoptify.ui.login.LoginViewModel
 import com.example.adoptify.ui.pet.PetViewModel
+import com.example.adoptify.ui.pet.abandoned.AbandonedActivity
+import com.example.adoptify.ui.pet.list.AbandonedPetAdapter
 import com.example.adoptify.ui.pet.list.ListPetActivity
+import com.example.adoptify.ui.shelter.DetailShelterActivity
+import com.example.adoptify.ui.shelter.ShelterViewModel
 import com.example.adoptify.ui.welcome.WelcomeActivity
 import com.example.adoptify.utils.ViewModelFactory
 
@@ -50,9 +50,7 @@ class HomeFragment : Fragment() {
         )
     }
 
-
     private var token = ""
-
 
 
     override fun onCreateView(
@@ -68,17 +66,23 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewPager = homeFragment.itemCaraousel
+        val viewPager: ViewPager2 = homeFragment.itemCaraousel
         val dotsPagerAdapter = homeFragment.dotsIndicator
+
+        shelterViewModel.getShelter()
 
         viewPager.apply {
             clipChildren = false
             clipToPadding = false
             (getChildAt(0) as RecyclerView).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
-            adapter = CaraouselAdapter(dummyBanner)
-        }
+            shelterViewModel.shelterResponse.observe(viewLifecycleOwner) {
+                val shelterList = shelterViewModel.shelterResponse.value ?: emptyList()
 
-        dotsPagerAdapter.attachTo(viewPager)
+                adapter = CaraouselAdapter(shelterList.take(5))
+
+                dotsPagerAdapter.attachTo(viewPager)
+            }
+        }
 
         val btnDonation = homeFragment.donation.btnDonation
         setDonationButtonClickListener(btnDonation, "donation")
@@ -90,11 +94,6 @@ class HomeFragment : Fragment() {
         val btnGecko = homeFragment.btnCategories.btnGecko
         val btnSugarGlider = homeFragment.btnCategories.btnSugarGlider
 
-//        btnCat.setOnClickListener {
-//            val intent = Intent(requireActivity(), ListPetActivity::class.java)
-//            intent.putExtra(CATEGORY, "kucing")
-//            startActivity(intent)
-//        }
 
         setCategoryButtonClickListener(btnCat, "kucing")
         setCategoryButtonClickListener(btnDog, "anjing")
@@ -112,6 +111,25 @@ class HomeFragment : Fragment() {
             }
         }
 
+        petViewModel.getAbandonedPet()
+
+        showRecyclerList()
+        petViewModel.abandonedResponse.observe(requireActivity()) {
+            val abandonedList = petViewModel.abandonedResponse.value ?: emptyList()
+
+            homeFragment.rvAbandoned.adapter = AbandonedPetAdapter(abandonedList.take(4))
+        }
+
+        homeFragment.btnAll.setOnClickListener {
+            val intent = Intent(requireActivity(), AbandonedActivity::class.java)
+            requireActivity().startActivity(intent)
+        }
+
+        homeFragment.icAll.setOnClickListener {
+            val intent = Intent(requireActivity(), DetailShelterActivity::class.java)
+            requireActivity().startActivity(intent)
+        }
+
     }
 
     fun setDonationButtonClickListener(btnDonation: ImageView, donation: String) {
@@ -122,12 +140,19 @@ class HomeFragment : Fragment() {
         }
     }
 
-
     fun setCategoryButtonClickListener(button: ImageView, category: String) {
         button.setOnClickListener {
             val intent = Intent(requireActivity(), ListPetActivity::class.java)
             intent.putExtra(CATEGORY, category)
             startActivity(intent)
+        }
+    }
+
+    private fun showRecyclerList() {
+        homeFragment.rvAbandoned.apply {
+            layoutManager =
+                LinearLayoutManager(requireActivity(), LinearLayoutManager.HORIZONTAL, false)
+            setHasFixedSize(true)
         }
     }
 
